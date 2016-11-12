3988494c6638ca9ece092f329107c3799d6f4b54
lucacervello <luca.cervello@gmail.com>
2016-11-12 17:34:18 +0100

    exercise 1 done

--- sequential_erlang/src/sequential_erlang.erl
+++ sequential_erlang/src/sequential_erlang.erl
@@ -1,36 +1,104 @@
 -module(sequential_erlang).
 
 %% API exports
--export([is_palindrome/1, is_an_anagram/2, is_proper/1]).
+-export([is_palindrome/1, is_an_anagram/2, factors/1, is_proper/1]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
--spec is_palindrome(string) -> bool.
+-spec is_palindrome(string()) -> boolean().
 is_palindrome(Str) ->
-    S = string:to_lower(Str),
-    Sr = lists:reverse(string:to_lower(Str)),
-    S == Sr.
+    clean(Str) == lists:reverse(clean(Str)).
 
+-spec is_an_anagram(string(), list(string())) -> boolean().
 is_an_anagram(Str, Lst) ->
-    true.
+    SortedStr = lists:sort(Str),
+    lists:any(fun (X) -> lists:sort(X) == SortedStr end, Lst).
 
+-spec factors(integer()) -> list(integer()).
+factors(N) ->
+    lists:filter(fun (X) -> N rem X == 0 andalso is_prime(X) end, lists:seq(1, N)).
+%[X || X <- lists:seq(1, N), is_prime(X)].
+
+-spec is_proper(integer()) -> boolean().
 is_proper(N) ->
-    true.
+    FN = factors(N),
+    lists:sum(FN) == lists:foldl(fun (X, Acc) -> X * Acc end, 1, FN).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
+clean(Str) ->
+    lists:filter(fun (X) -> lists:member(X, lists:seq($a, $z)) end, string:to_lower(Str)).
+
+is_prime(1) ->
+    true;
+is_prime(2) ->
+    true;
+is_prime(N) ->
+    length(lists:filter(fun (X) -> N rem X == 0 end, lists:seq(1, N))) == 2.
 
 %% Test
 -ifdef(TEST).
+
 -include_lib("eunit/include/eunit.hrl").
 
-basic_test_() ->
-    fun () -> ?assert(1 + 1 =:= 2) end.
+%%%%%%%%%%%%%%%%%%%%%
+%% Palindrome test %%
+%%%%%%%%%%%%%%%%%%%%%
 
-anagram_simple_test() ->
+palindrome_simple_test() ->
     ?assert(is_palindrome("anna")).
+
+palindrome_not_so_simple_test() ->
+    ?assert(is_palindrome("detartrated")).
+
+palindrome_long_test() ->
+    ?assert(is_palindrome("Do geese see God?")).
+
+palindrome_another_long_test() ->
+    ?assert(is_palindrome("Rise to vote, sir.")).
+
+%%%%%%%%%%%%%%%%%%
+%% Anagram test %%
+%%%%%%%%%%%%%%%%%%
+
+no_matches_test() ->
+  ?assertEqual(false, is_an_anagram("diaper", ["hello", "world", "zombies", "pants"])).
+
+detect_simple_anagram_test() ->
+  ?assert(is_an_anagram("ant", ["tan", "stand", "at"])).
+
+does_not_confuse_different_duplicates_test() ->
+  ?assertEqual(false, is_an_anagram("galea", ["eagle"])).
+
+eliminate_anagram_subsets_test() ->
+  ?assertEqual(false, is_an_anagram("good", ["dog", "goody"])).
+
+detect_anagram_test() ->
+  ?assert(is_an_anagram("listen", ["enlists", "google", "inlets", "banana"])).
+
+multiple_anagrams_test() ->
+  ?assert(is_an_anagram("allergy", ["gallery", "ballerina", "regally", "clergy", "largely", "leading"])).
+
+simple_test() ->
+    ?assert(is_prime(5)).
+
+%% Factors test
+
+simple_factors_test() ->
+    ?assertEqual([1,2,3], factors(6)).
+
+another_simple_factors_test() ->
+    ?assertEqual([1,3], factors(9)).
+
+%% Proper test
+
+simple_proper_test() ->
+    ?assert(is_proper(6)).
+
+other_proper_test() ->
+    ?assertEqual(false, is_proper(9)).
 
 -endif.
