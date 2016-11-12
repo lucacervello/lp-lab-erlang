816c961f7c1975878adb1aeb62419467836b9e23
lucacervello <luca.cervello@gmail.com>
2016-11-12 14:36:09 +0100

    it fails ?

--- sequential_erlang/src/sequential_erlang.erl
+++ sequential_erlang/src/sequential_erlang.erl
@@ -1,13 +1,39 @@
 -module(sequential_erlang).
 
 %% API exports
--export([]).
+-export([is_palindrome/1, is_an_anagram/2, is_proper/1]).
 
 %%====================================================================
 %% API functions
 %%====================================================================
+-spec is_palindrome(string) -> bool.
+is_palindrome(Str) ->
+    S = string:to_lower(Str),
+    Sr = lists:reverse(string:to_lower(Str)),
+    S == Sr.
 
+is_an_anagram(Str, Lst) ->
+    true.
+
+is_proper(N) ->
+    true.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+
+%% Test
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+basic_test_() ->
+    fun () -> ?assert(1 + 1 =:= 2) end.
+
+anagram_simple_test() ->
+    ?assert(is_palindrome("anna")).
+
+fail_test() ->
+    ?assert(false).
+
+-endif.
